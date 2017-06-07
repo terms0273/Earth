4522fef9bbe4e4352a1468283753dfa188bfafe2
TTCAD\y-aiyoshizawa <y-aiyoshizawa@ttc-net.co.jp>
2017-06-07 16:08:51 +0900

    円状にアイコンを表示　時間を表示

--- scripts/src/main.js
+++ scripts/src/main.js
@@ -1,20 +1,29 @@
 import Weather from "./weather";
 import Map from "./map";
+import OneDayWeatherForecast from "./oneDayWeatherForecast";
 
 $(function() {
-    let weather = new Weather();
-    let map = new Map();
+  let weather = new Weather();
+  let map = new Map();
+  let oneDay = new OneDayWeatherForecast();
 
-    $("#search-city").click(updateWeather);
-    $("#input-city").keydown((e) =>{
-        if(e.keyCode == 13){
-            updateWeather();
-        }
-    });
-    weather.send(weather.city);
-
-    function updateWeather(){
-        let newCity = $("#input-city").val();
-        weather.send(newCity,map);
+  $("#search-city").click(updateWeather);
+  $("#tab-weather-forecast").click(()=>{
+    d3.select("#one-day svg").remove();
+    oneDay.init(weather.city);
+  });
+  $("#input-city").keydown((e) =>{
+    if(e.keyCode == 13){
+      updateWeather();
     }
+  });
+  weather.send(weather.city);
+
+  function updateWeather(){
+    let newCity = $("#input-city").val();
+    weather.send(newCity,map);
+  }
+
+
+
 });
---
+++ scripts/src/oneDayWeatherForecast.js
@@ -0,0 +1,97 @@
+import * as d3 from "d3";
+
+export default class OneDayWeatherForecast{
+  constructor(){}
+
+  init(cityName){
+    let url = "http://api.openweathermap.org/data/2.5/forecast?q=" +
+    cityName +
+    "&appid=9ab6492bf227782c3c7ae7417a624014";
+    $.ajax({
+        url:url
+    }).then((json) =>{
+
+      this.print(json);  //成功時
+    },
+      (err) =>{
+        console.log(err);
+      }
+    );
+  }
+  print(json){
+    console.log(json);
+
+    var width = 760;
+    var height = 600;
+    var radius = 100;
+    var margin = 50;
+
+    var svg = d3.select("#one-day").append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+    // translateで画面中央に移動。
+    var translated = svg.append("g")
+      .attr("transform","translate(" + (radius + margin) +","+ (radius + margin) +")");
+
+
+    // var data = [45,90,135,180,225,270,315,360];
+    var data = [];
+    for(var i = 0; i < 8; i++){
+      data[i] = json.list[i];
+    }
+
+    // d3.extent()で、データの最大値と最小値が[最大値,最小値]で出る。
+    var dataExtent = d3.extent(data);
+
+    // データリストの位置が0で角度0、最後の位置で360度になるように均等に角度を割りふる。
+    var r = d3.scale.linear()
+      .domain([0,data.length])
+      .range([180,540]);
+
+    var preRotated = translated.selectAll(".pre_rotated").data(data).enter()
+      .append("g")
+      .attr("class","pre_rotated");
+
+    var circles = preRotated.append("image")
+      .attr({
+        x:0,
+        y:0
+      })
+      .transition()
+        .delay(function(d,i){
+        return i * 50;
+      })
+      .duration(500)
+      .ease("linear")
+      .attr({
+        transform:function(d,i){ return "rotate(" + 360 + ")"; },
+        x:function(d,i){return Math.cos(r(i) * Math.PI / 180) * radius},
+        y:function(d,i){return Math.sin(r(i) * Math.PI / 180) * radius},
+        href: function(d,i){return "http://openweathermap.org/img/w/"+d.weather[0].icon+".png"}
+      });
+
+      var circles = preRotated.append("text")
+        .attr({
+          x:0,
+          y:0
+        })
+        .transition()
+          .delay(function(d,i){
+          return i * 50;
+        })
+        .duration(500)
+        .ease("linear")
+        .attr({
+          transform:function(d,i){ return "rotate(" + 360 + ")"; },
+          x:function(d,i){return Math.cos(r(i) * Math.PI / 180) * radius},
+          y:function(d,i){return Math.sin(r(i) * Math.PI / 180) * radius}
+        })
+        .text(function(d){return new Date(d.dt * 1000).getHours() + ":00"});
+
+
+  }
+  rotate(){
+
+  }
+}
