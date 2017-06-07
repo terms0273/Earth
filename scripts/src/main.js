5385fe20298a6f977c32029e4b954aa71f67177c
TTCAD\d-nakamura <d-nakamura@ttc-net.co.jp>
2017-06-07 19:10:21 +0900

    d3の環境構築済み、アイコン表示途中

--- scripts/src/main.js
+++ scripts/src/main.js
@@ -1,9 +1,11 @@
 import Weather from "./weather";
 import Map from "./map";
+import WeeklyWeatherForecast from "./weeklyWeatherForecast";
 
 $(function() {
     let weather = new Weather();
     let map = new Map();
+    let weekly = new WeeklyWeatherForecast();
 
     $("#search-city").click(updateWeather);
     $("#input-city").keydown((e) =>{
@@ -17,4 +19,8 @@ $(function() {
         let newCity = $("#input-city").val();
         weather.send(newCity,map);
     }
+
+    $("#tab-weather-forecast").click(() =>{
+      weekly.send(weather.city);
+    });
 });
