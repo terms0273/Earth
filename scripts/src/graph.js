f77fa0999912cd1d7dcd8d54bfd51790e32e3a79
Ryota Tominaga <r-tominaga@ttc-net.co.jp>
2017-06-08 18:27:13 +0900

    グラフ4つ表示成功

--- scripts/src/graph.js
+++ scripts/src/graph.js
@@ -4,21 +4,19 @@ export default class Graph{
   constructor(){
     ;
   }
-  init(json){
+  print(json){
     var forecastlist = json.list;
     var margin = {top: 20, right: 20, bottom: 30, left: 40};
-    var w = 800 - margin.left - margin.right;
+    var w = 1000 - margin.left - margin.right;
     var h = 700 - margin.top - margin.bottom;
     var padding = 20;
 
-
-
     var xScale = d3.scale.linear()
-                    .domain([0,forecastlist.length])
-                    .range([padding, w-padding]);
+                    .domain([forecastlist[0].dt, forecastlist[forecastlist.length-1].dt])
+                    .range([padding, w-margin.left]);
 
     var yScale = d3.scale.linear()
-                    .domain([0, 50])
+                    .domain([0, 100])
                     .range([h-padding, padding]);
 
     var svg = d3.select("#graph")
@@ -29,16 +27,36 @@ export default class Graph{
 
     var xAxis = d3.svg.axis()
                     .scale(xScale)
-                    .orient("bottom");
+                    .orient("bottom")
+                    .tickFormat(function(d){
+                        var date = new Date(d * 1000);
+                        return (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getHours() + " :00";
+                      });
 
     var yAxis = d3.svg.axis()
                     .scale(yScale)
                     .orient("left");
+    svg.append("g")
+       .attr({
+         class:"y axis",
+         transform: "translate(20, 0)"})
+       .call(yAxis);
 
-    var d3line = d3.svg.line()
-      .x(function(d,i){return xScale(i);})
-      .y(function(d){return yScale(d.main.temp - 273.15);})
-      .interpolate("cardinal");
+    svg.append("g")
+       .attr({
+         class:"x axis",
+         transform: "translate(0, 630)"})
+       .call(xAxis);
+
+
+    //ここまではグラフの共通部分
+    //ここから4つのグラフにチェックボックスのチェック有無で分岐
+    if($("#chkTemp:checked").val()){
+      var d3line = d3.svg.line()
+        .x(function(d){return xScale(d.dt);})
+        .y(function(d){return yScale(d.main.temp - 273.15);})
+        .interpolate("cardinal");
+
 
       svg.append("path")
         .attr("d", d3line(forecastlist))
@@ -48,44 +66,147 @@ export default class Graph{
         .style("stroke", "steelblue")
         .style("fill", "none");
 
-      svg.append("g")
-         .attr({
-           class:"y axis",
-           transform: "translate(20, 0)"})
-         .call(yAxis);
 
-      svg.append("g")
-      .attr({
-        class:"x axis",
-        transform: "translate(0, 630)"})
-      .call(xAxis);
+      var circle = svg.selectAll("circle")
+                .data(forecastlist)
+                .enter()
+                .append("circle")
+       .attr({
+         // enterに入っているデータ一つ一つで下の処理を行う
+         'cx': function(d){return xScale(d.dt);},
+         'cy': function(d){return yScale(d.main.temp - 273.15);},
+         'r': function(d) { return 2; },
+         transform: "translate(0, 0)"
+       });
+    }
 
-        var circle = svg.selectAll('circle')
-                  .data(forecastlist)
-                  .enter()
-                  .append('circle')
-         .attr({
-           // enterに入っているデータ一つ一つで下の処理を行う
-           'cx': function(d,i){return xScale(i);},
-           'cy': function(d){return yScale(d.main.temp - 273.15);},
-           'r': function(d) { return 2; },
-           transform: "translate(0, 0)"
-         });
+
+    //湿度チェック
+    if($("#chkHumide:checked").val()){
+      var d3line = d3.svg.line()
+        .x(function(d){return xScale(d.dt);})
+        .y(function(d){return yScale(d.main.humidity);})
+        .interpolate("cardinal");
+
+
+      svg.append("path")
+        .attr("d", d3line(forecastlist))
+        .attr({
+          transform: "translate(0, 0)"})
+        .style("stroke-width", 2)
+        .style("stroke", "steelblue")
+        .style("fill", "none");
+
+
+      var circle = svg.selectAll("circle2")
+                .data(forecastlist)
+                .enter()
+                .append("circle")
+       .attr({
+         // enterに入っているデータ一つ一つで下の処理を行う
+         'cx': function(d){return xScale(d.dt);},
+         'cy': function(d){return yScale(d.main.humidity);},
+         'r': function(d) { return 2; },
+         transform: "translate(0, 0)"
+       });
+    }
+
+    //体感温度チェック
+    if($("#chkTaikan:checked").val()){
+
+
+      var d3line = d3.svg.line()
+        .x(function(d){return xScale(d.dt);})
+        .y(function(d){
+          var v = Math.pow(d.wind.speed, 0.75);
+          var a = 1.76 + 1.4*v;
+          return yScale(37-(37-(d.main.temp-273.15))/(0.68 - (0.0014)*(d.main.humidity) + (1/a)));
+        })
+        .interpolate("cardinal");
+
+
+      svg.append("path")
+        .attr("d", d3line(forecastlist))
+        .attr({
+          transform: "translate(0, 0)"})
+        .style("stroke-width", 2)
+        .style("stroke", "steelblue")
+        .style("fill", "none");
+
+
+      var circle = svg.selectAll("circle3")
+                .data(forecastlist)
+                .enter()
+                .append("circle")
+       .attr({
+         // enterに入っているデータ一つ一つで下の処理を行う
+         'cx': function(d){return xScale(d.dt);},
+         'cy': function(d){
+           var v = Math.pow(d.wind.speed, 0.75);
+           var a = 1.76 + 1.4*v;
+           return yScale(37-(37-(d.main.temp-273.15))/(0.68 - (0.0014)*(d.main.humidity) + (1/a)));
+         },
+         'r': function(d) { return 2; },
+         transform: "translate(0, 0)"
+       });
+    }
+
+    if($("#chkFukai:checked").val()){
+      var d3line = d3.svg.line()
+        .x(function(d){return xScale(d.dt);})
+        .y(function(d){
+          return yScale(0.81*(d.main.temp - 273.15) + 0.01*d.main.humidity*(0.99*(d.main.temp - 273.15) - 14.3)+46.3);
+        })
+        .interpolate("cardinal");
+
+
+      svg.append("path")
+        .attr("d", d3line(forecastlist))
+        .attr({
+          transform: "translate(0, 0)"})
+        .style("stroke-width", 2)
+        .style("stroke", "steelblue")
+        .style("fill", "none");
+
+
+      var circle = svg.selectAll("circle4")
+                .data(forecastlist)
+                .enter()
+                .append("circle")
+       .attr({
+         // enterに入っているデータ一つ一つで下の処理を行う
+         'cx': function(d){return xScale(d.dt);},
+         'cy': function(d){
+           return yScale(0.81*(d.main.temp - 273.15) + 0.01*d.main.humidity*(0.99*(d.main.temp - 273.15) - 14.3) + 46.3);
+         },
+         'r': function(d) { return 2; },
+         transform: "translate(0, 0)"
+       });
+    }
+
   }
 
-  send(cityName){
-      let url = "http://api.openweathermap.org/data/2.5/forecast?q=" +
-      cityName +
-      "&appid=9ab6492bf227782c3c7ae7417a624014";
 
-      $.ajax({
-          url:url
-      }).then((json) =>{
-          console.log(json);
-          this.init(json);
-      },(err) =>{
-          console.log(err);
-      });
 
+  hideGraph(){
+    d3.select("path").remove();
+    d3.select("circle").remove();
   }
+
+  init(cityName){
+    d3.select("svg").remove();
+    let url = "http://api.openweathermap.org/data/2.5/forecast?q=" +
+    cityName +
+    "&appid=9ab6492bf227782c3c7ae7417a624014";
+
+    $.ajax({
+        url:url
+    }).then((json) =>{
+        console.log(json);
+        this.print(json);
+    },(err) =>{
+        console.log(err);
+    });
+  }
+
 }
--- scripts/src/main.js
+++ scripts/src/main.js
@@ -6,20 +6,35 @@ import Graph from "./graph";
 $(function() {
     let weather = new Weather();
     let map = new Map();
+    let graph = new Graph();
 
+    //検索ボタン押したときに呼ぶ
     $("#search-city").click(updateWeather);
+
+    //グラフボタン押したとき展開
     $("#tag-graph").click(() =>{
-      let graph = new Graph();
-      graph.send(weather.city);
+      if($("#tag-graph").parent().attr("aria-expanded") === "false"){
+        ;
+      }
     });
+
+    //チェックボックスにチェック
+    $(":checkbox").click(()=>{
+      graph.init(weather.city);
+    });
+
+    //エンターボタンで検索動作
     $("#input-city").keydown((e) =>{
         if(e.keyCode == 13){
             updateWeather();
         }
     });
     weather.send(weather.city);
+
+    //検索したときの動作
     function updateWeather(){
         let newCity = $("#input-city").val();
         weather.send(newCity,map);
+        setTimeout(function(){graph.init(weather.city)}, 500);
     }
 });
