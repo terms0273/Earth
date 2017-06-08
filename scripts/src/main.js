4953b9be959251d73799d0d8e04608f8321167e5
TTCAD\d-nakamura <d-nakamura@ttc-net.co.jp>
2017-06-08 18:27:29 +0900

    週間天気予報作成

--- scripts/src/main.js
+++ scripts/src/main.js
@@ -3,24 +3,27 @@ import Map from "./map";
 import WeeklyWeatherForecast from "./weeklyWeatherForecast";
 
 $(function() {
-    let weather = new Weather();
-    let map = new Map();
-    let weekly = new WeeklyWeatherForecast();
+  let weather = new Weather();
+  let map = new Map();
+  let weekly = new WeeklyWeatherForecast();
 
-    $("#search-city").click(updateWeather);
-    $("#input-city").keydown((e) =>{
-        if(e.keyCode == 13){
-            updateWeather();
-        }
-    });
-    weather.send(weather.city);
-
-    function updateWeather(){
-        let newCity = $("#input-city").val();
-        weather.send(newCity,map);
+  $("#search-city").click(updateWeather);
+  $("#input-city").keydown((e) =>{
+    if(e.keyCode == 13){
+      updateWeather();
     }
+  });
+  weather.send(weather.city);
 
-    $("#tab-weather-forecast").click(() =>{
-      weekly.send(weather.city);
-    });
+    let newCity = $("#input-city").val();
+    function updateWeather(){
+    weather.send(newCity,map);
+    setTimeout(()=>{weekly.init(weather.city);}, 500);
+  }
+
+  $("#tab-weather-forecast").click(() =>{
+    if($("#tab-weather-forecast").parent().attr("aria-expanded") === "false"){
+      weekly.init(weather.city);
+    }
+  });
 });
--- scripts/src/weeklyWeatherForecast.js
+++ scripts/src/weeklyWeatherForecast.js
@@ -5,44 +5,114 @@ export default class WeeklyWeatherForecast {
 
   }
 
-  init(json) {
-    var w = 500;
-    var h = 200;
-    var padding = 20;
+  print(json) {
+    let w = 850;
+    let h = 200;
+    let padding = 20;
 
-    var svg = d3.select("body").append("svg").attr({width:w, height:h});
+    let weekday = [];
 
-    var data = [];
-    for (var i = 0; i < data.length; i++) {
-      data[i] = json.list[i];
+    weekday[0] = "Sun";
+    weekday[1] = "Mon";
+    weekday[2] = "Tue";
+    weekday[3] = "Wed";
+    weekday[4] = "Thu";
+    weekday[5] = "Fri";
+    weekday[6] = "Sat";
+
+    let indexData = [];
+
+    indexData[0] = "DATE";
+    indexData[1] = "WEATHER";
+    indexData[2] = "MAX";
+    indexData[3] = "MIN";
+
+    let yPoint = [];
+
+    yPoint[0] = 30;
+    yPoint[1] = 70;
+    yPoint[2] = 140;
+    yPoint[3] = 170;
+
+    let svg = d3.select("#weekly").append("svg").attr({width:w, height:h});
+
+    let dataset = [];
+    for (let i = 0; i < json.list.length; i++) {
+      dataset[i] = json.list[i];
     }
 
-    var imgs = d3.selectAll("")
-    .data(data)
+    let xscale = d3.scale.linear()
+    .domain([0,dataset.length])
+    .range([padding,w-padding]);
+
+    let weekly = svg.append("g").selectAll("g")
+    .data(dataset)
+    .enter();
+
+    let index = svg.append("g").selectAll("g")
+    .data(indexData)
     .enter()
-    .append('img')
+    .append("text");
+
+    index.attr({
+      x: 0,
+      y: function(d,i) {return yPoint[i];}
+    })
+    .text(function(d) {return d;});
+
+    weekly.append("text")
     .attr({
-      'src'   : function(data){
+      x: function(d,i){return xscale(i) + 70;},
+      y: function() {return yPoint[0];}
+    })
+    .text(function(d) {
+      let date = new Date(d.dt * 1000);
+      return (date.getMonth() + 1) + "/" +
+      date.getDate() +
+      "(" + weekday[date.getDay()] + ")";
+    });
+
+    weekly.append("image")
+    .attr({
+      href: function(d){
         return 'http://openweathermap.org/img/w/'+d.weather[0].icon+'.png';
       },
-      'width' : 50,
-      'height': 50,
+      x: function(d,i){return xscale(i) + 70;},
+      y: function() {return yPoint[1];}
+    });
 
+    weekly.append("text")
+    .attr({
+      x: function(d,i){return xscale(i) + 85;},
+      y: function() {return yPoint[2];}
+    })
+    .text(function(d) {
+      return Math.round(d.temp.max - 273.15);
+    });
+
+    weekly.append("text")
+    .attr({
+      x: function(d,i){return xscale(i) + 85;},
+      y: function() {return yPoint[3];}
+    })
+    .text(function(d) {
+      return Math.round(d.temp.min - 273.15);
     });
   }
 
-  send(cityName) {
+  init(cityName) {
+    d3.select("svg").remove();
     let url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" +
     cityName +
     "&appid=9ab6492bf227782c3c7ae7417a624014";
 
     $.ajax({
-        url:url
+      url:url
     }).then((json) =>{
-        console.log(json);
-        this.init(json);
+      console.log(json);
+      this.print(json);
     },(err) =>{
-        console.log(err);
+      console.log(err);
     });
   }
 }
