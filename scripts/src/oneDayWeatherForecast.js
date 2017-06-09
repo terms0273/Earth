b9ce38c3042e52b9da0c8f26b13c6fa861a64d84
TTCAD\y-aiyoshizawa <y-aiyoshizawa@ttc-net.co.jp>
2017-06-09 13:25:10 +0900

    レビュー内容を修正

--- scripts/src/oneDayWeatherForecast.js
+++ scripts/src/oneDayWeatherForecast.js
@@ -5,7 +5,6 @@ export default class OneDayWeatherForecast{
 
   init(weather){
 
-    //TODO再描画処理にしたほうがいいかも
     d3.select("#one-day svg").remove();
 
     //メンバー変数の初期化
@@ -18,7 +17,7 @@ export default class OneDayWeatherForecast{
     weather.cityName +
     "&appid=9ab6492bf227782c3c7ae7417a624014";
     $.ajax({
-        url:url
+      url:url
     }).then((json) =>{
       this.json = json;
       this.print();  //成功時
@@ -42,14 +41,16 @@ export default class OneDayWeatherForecast{
 
     // translateで画面中央に移動。
     let translated = svg.append("g")
-      .attr("transform","translate(" + (radius + margin) +","+ (radius + margin) +")");
-
+      .attr("transform","translate("
+        + (radius + margin) +","
+        + (radius + margin) +")"
+      );
 
     //データの初期化
     let data = [];
     //表示しないアイコン数
     let notDataCount = new Date(this.json.list[0].dt * 1000).getHours() / 3 - 1;
-    //21字以降だと-1になってしまうので
+    //21時以降だと-1になってしまうので
     if(notDataCount == -1){
       notDataCount = 7
     }
@@ -68,6 +69,7 @@ export default class OneDayWeatherForecast{
     let rotated = translated.selectAll("g").data(data).enter()
       .append("g");
 
+    //天気予報のアイコンを表示する
     let image = rotated.append("image")
       .attr({
         x:0,
@@ -84,13 +86,14 @@ export default class OneDayWeatherForecast{
         y:(d,i) => {return Math.sin(rScale(i) * Math.PI / 180) * radius},
         href:(d) => {
           if(d === null){
-            return "/assets/images/finished-icon.png" //TODO:過ぎた予報のイメージ画像
+            return "/assets/images/finished-icon.png"
           }else{
             return "http://openweathermap.org/img/w/"+d.weather[0].icon+".png";
           }
         }
       });
 
+    //時間をテキストで表示する 例）21:00
     let nowScale = d3.scale.linear()
       .domain([0,24])
       .range([90,450]);
@@ -107,11 +110,16 @@ export default class OneDayWeatherForecast{
       .duration(500)
       .ease("linear")
       .attr({
-        x:(d,i) => {return Math.cos(rScale(i) * Math.PI / 180) * (radius + 40) + 3},
-        y:(d,i) => {return Math.sin(rScale(i) * Math.PI / 180) * (radius + 40) + 30}
+        x:(d,i) => {
+          return Math.cos(rScale(i) * Math.PI / 180) * (radius + 40) + 3;
+        },
+        y:(d,i) => {
+          return Math.sin(rScale(i) * Math.PI / 180) * (radius + 40) + 30;
+        }
       })
       .text((d,i) => {return (i * 3 + 3  % 24) + ":00"});
 
+    //現在の太陽or月を表示する
     let nowIcon = translated.append("image")
       .attr({
         x:0,
@@ -121,33 +129,41 @@ export default class OneDayWeatherForecast{
       .duration(500)
       .ease("linear")
       .attr({
-        x:() => {return Math.cos(nowScale(this.date.getHours()) * Math.PI / 180) * (radius - 50)},
-        y:() => {return Math.sin(nowScale(this.date.getHours()) * Math.PI / 180) * (radius - 50)},
+        x:() => {
+          return Math.cos(nowScale(this.date.getHours()) * Math.PI / 180)
+            * (radius - 50);
+        },
+        y:() => {
+          return Math.sin(nowScale(this.date.getHours()) * Math.PI / 180)
+            * (radius - 50);
+        },
         href: () => {
           if(this.isSun()){
-            return "http://openweathermap.org/img/w/01d.png"
+            return "http://openweathermap.org/img/w/01d.png";
           }else{
-            return "http://openweathermap.org/img/w/01n.png"
+            return "http://openweathermap.org/img/w/01n.png";
           }
         }
       });
+
+    //真ん中に点をつける
     let center = translated.append("circle")
       .attr({
-        cx:()=>{return 25},
-        cy:()=>{return 25},
+        cx:()=>{return 25;},
+        cy:()=>{return 25;},
         r:2,
         fill:"orange"
       });
 
-    //TODO:日本以外の都市でバグることがある
+    //円グラフで日の出、日没を表現する
     let risedt =this.sunrise;
     let setdt = this.sunset;
     let deySecond = 24 * 60 * 60;
-    var dataArr = [
+    let dataArr = [
        {score: setdt - risedt},
        {score: deySecond - (setdt - risedt)}
     ];
-    var colors = [
+    let colors = [
       "255, 0, 0",
       "0, 0, 255"
     ];
@@ -156,36 +172,41 @@ export default class OneDayWeatherForecast{
       .range([180,540]);
 
     //合計値を算出する上でのラムダ．
-    var actGettingScore = function(d){return d.score};
+    let actGettingScore = function(d){return d.score};
     //arcオブジェクトは扇形に相当するd操作を生成する．
-    var arc = d3.svg.arc()
+    let arc = d3.svg.arc()
       .startAngle(function(d){return 0;})
       .endAngle(function(d){return Math.PI * 2 * d.score/ deySecond; })
       .innerRadius(function(d){return 20;})
       .outerRadius(function(d){return radius + 20;});
 
     let temp = new Date(risedt * 1000);
-    let riseSecond = temp.getHours() * 3600 + temp.getMinutes() * 60 + temp.getSeconds();
-    var circleGraph = translated.append("g");
+    let riseSecond =
+      temp.getHours() * 3600 + temp.getMinutes() * 60 + temp.getSeconds();
+    let circleGraph = translated.append("g");
     circleGraph.selectAll("path")
       .data(dataArr)
       .enter()
       .append("path")
       .attr({
-        fill: (d,i) => {return "rgba("+colors[i]+", 0.0)"}
+        fill: (d,i) => {return "rgba("+colors[i]+", 0.0)";}
       })
       .transition()
       .duration(500)
       .ease("linear")
       .attr({
-        d: (d,i) => {return arc(d)},
+        d: (d,i) => {return arc(d);},
         transform: (d,i) => {
           //累計値を計算して，回転角に変換する．
-          var subArr = i == 0 ? []: dataArr.slice(0, i);
-        return "translate(25,25),rotate(" + 360 * d3.sum(subArr, actGettingScore)/deySecond + "),rotate("+riseScale(riseSecond)+")"
+          let subArr = i == 0 ? []: dataArr.slice(0, i);
+          return "translate(25,25),rotate("
+                    + 360 * d3.sum(subArr, actGettingScore) / deySecond +
+                 "),rotate("
+                    + riseScale(riseSecond) +
+                 ")";
         },
         stroke: "white",
-        fill: (d,i) => {return "rgba("+colors[i]+", 0.1)"}
+        fill: (d,i) => {return "rgba("+colors[i]+", 0.1)";}
       });
   }
 
