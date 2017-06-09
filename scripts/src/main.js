57caefec2c41bce6b9e9e075cfee2c8cb7250583
TTCAD\y-aiyoshizawa <y-aiyoshizawa@ttc-net.co.jp>
2017-06-09 16:07:00 +0900

    バグの修正

--- scripts/src/main.js
+++ scripts/src/main.js
@@ -9,10 +9,10 @@ $(function() {
   let map = new Map();
   let graph = new Graph();
   let oneDay = new OneDayWeatherForecast();
-  
+
   //検索ボタン押したときに呼ぶ
   $("#search-city").click(updateWeather);
-  
+
   $("#tab-weather-forecast").click(()=>{
     if($("#tab-weather-forecast").parent().attr("aria-expanded") === "false"){
       d3.select("svg").remove();
@@ -23,7 +23,7 @@ $(function() {
   $(":checkbox").click(()=>{
     graph.init(weather.city);
   });
-  
+
   //エンターボタンで検索動作
   $("#input-city").keydown((e) =>{
     if(e.keyCode == 13){
@@ -32,20 +32,20 @@ $(function() {
   });
   weather.send(weather.city);
   setTimeout(()=>{oneDay.init(weather);}, 200);
-  
+
   //検索したときの動作
   function updateWeather(){
     let newCity = $("#input-city").val();
     weather.send(newCity,map);
-    setTimeout(function(){graph.init(weather.city)}, 500);   
-  });
-  
+    setTimeout(function(){graph.init(weather.city)}, 500);
+  };
+
   $("#input-city").keydown((e) =>{
     if(e.keyCode == 13){
       updateWeather();
     }
   });
-  
+
   function updateWeather(){
     let newCity = $("#input-city").val();
     weather.send(newCity,map);
