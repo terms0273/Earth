eba7b78659cf25ea95ff7e65d37c7c6f2042c519
yuta <aiyoshizawa.yuta@gmail.com>
2017-06-13 08:19:56 +0900

    リファクタリングの途中

--- scripts/src/oneDayWeatherForecast.js
+++ scripts/src/oneDayWeatherForecast.js
@@ -45,135 +45,34 @@ export default class OneDayWeatherForecast{
   print(){
     d3.select("#one-day > svg").remove();
 
-    let width = 760;
-    let height = 350;
-    let radius = 100;
-    let margin = 50;
+    this.width = 760;
+    this.height = 350;
+    this.radius = 100;
+    this.margin = 50;
 
     let svg = d3.select("#one-day").append("svg")
-      .attr("width", width)
-      .attr("height", height);
+      .attr("width", this.width)
+      .attr("height", this.height);
 
     // translateで画面中央に移動。
     let translated = svg.append("g")
       .attr("transform","translate("
-        + (radius + margin) +","
-        + (radius + margin + 20) +")"
+        + (this.radius + this.margin) +","
+        + (this.radius + this.margin + 20) +")"
       );
 
     // 360度になるように均等に角度を割りふる。
-    let rScale = d3.scale.linear()
+    this.rScale = d3.scale.linear()
       .domain([0,24])
       .range([90,450]);
 
-    //日付を表示する
-    let dateFormat = require('dateformat');
-    let now = svg.append("g")
-      .selectAll("text")
-      .data([this.forecastList[0]])
-      .enter()
-      .append("text")
-      .attr({
-        x:radius + margin,
-        y:20
-      })
-      .text((d) => {
-        return dateFormat(new Date(d.dt * 1000 - 1 + this.timeZone)
-               .toUTCString(),"UTC:m/d(ddd)");
-      });
+    this.datePrint(svg);
 
-
-    //天気予報のアイコンを表示する
     let rotated = translated.selectAll("g").data(this.forecastList).enter()
       .append("g");
-    let image = rotated.append("image")
-      .attr({
-        x:0,
-        y:0
-      })
-      .transition()
-      .delay((d,i) => {
-        return i * 50;
-      })
-      .duration(500)
-      .ease("linear")
-      .attr({
-        x:(d,i) => {return Math.cos(rScale(new Date(d.dt * 1000 + this.timeZone).getUTCHours()) * Math.PI / 180) * radius},
-        y:(d,i) => {return Math.sin(rScale(new Date(d.dt * 1000 + this.timeZone).getUTCHours()) * Math.PI / 180) * radius},
-        href:(d) => {
-          if(typeof d.weather === "undefined"){
-            return "/assets/images/finished-icon.png";
-          }else{
-            let dn = "d";
-            if(!this.isSun(d.dt + this.timeZone / 1000)){
-              dn = "n";
-            }
-            let iconName = d.weather[0].icon
-                          .slice(0,d.weather[0].icon.length - 1)
-                          + dn;
-            return "http://openweathermap.org/img/w/"+iconName+".png";
-          }
-        }
-      });
-
-    //時間をテキストで表示する 例）21:00
-    let nowScale = d3.scale.linear()
-      .domain([0,24])
-      .range([90,450]);
-
-    let text = rotated.append("text")
-      .attr({
-        x:0,
-        y:0
-      })
-      .transition()
-      .delay((d,i) => {
-        return i * 50;
-      })
-      .duration(500)
-      .ease("linear")
-      .attr({
-        x:(d,i) => {
-          return Math.cos(rScale(new Date(d.dt * 1000 + this.timeZone).getUTCHours()) * Math.PI / 180) * (radius + 40) + 3;
-        },
-        y:(d,i) => {
-          return Math.sin(rScale(new Date(d.dt * 1000 + this.timeZone).getUTCHours()) * Math.PI / 180) * (radius + 40) + 30;
-        }
-      })
-      .text((d,i) => {
-        var dateFormat = require('dateformat');
-        return new Date(d.dt * 1000 + this.timeZone).getUTCHours() + ":00";
-      });
-
-    //現在の太陽or月を表示する
-    let nowIcon = translated.append("image")
-      .attr({
-        x:0,
-        y:0
-      })
-      .transition()
-      .duration(500)
-      .ease("linear")
-      .attr({
-        x:() => {
-          return Math.cos(nowScale(this.date.getUTCHours()) * Math.PI / 180)
-                 * (radius - 50);
-        },
-        y:() => {
-          return Math.sin(nowScale(this.date.getUTCHours()) * Math.PI / 180)
-                 * (radius - 50);
-        },
-        href: () => {
-          let tempDate = new Date(this.forecastList[0].dt * 1000 + this.timeZone - 1);
-          if(this.date.getUTCDate() !== tempDate.getUTCDate()){
-            return null;
-          }else if(this.isSun(this.date.getTime() / 1000)){
-            return "http://openweathermap.org/img/w/01d.png";
-          }else{
-            return "http://openweathermap.org/img/w/01n.png";
-          }
-        }
-      });
+    this.forecastIconPrint(rotated);
+    this.timeTextPrint(rotated);
+    this.nowIconPrint(translated);
 
     //真ん中に点をつける
     let center = translated.append("circle")
@@ -184,67 +83,9 @@ export default class OneDayWeatherForecast{
         fill:"orange"
       });
 
-    //円グラフで日の出、日没を表現する
-    let risedt =this.sunrise;
-    let setdt = this.sunset;
-    let deySecond = 24 * 60 * 60;
-    let dataArr = [
-       {score: setdt - risedt},
-       {score: deySecond - (setdt - risedt)}
-    ];
-    let colors = [
-      "255, 0, 0",
-      "0, 0, 255"
-    ];
-    let riseScale = d3.scale.linear()
-      .domain([0,deySecond])
-      .range([180,540]);
-
-    //合計値を算出する上でのラムダ．
-    let actGettingScore = function(d){return d.score;};
-    //arcオブジェクトは扇形に相当するd操作を生成する．
-    let arc = d3.svg.arc()
-      .startAngle(function(d){return 0;})
-      .endAngle(function(d){return Math.PI * 2 * d.score/ deySecond;})
-      .innerRadius(function(d){return 20;})
-      .outerRadius(function(d){return radius + 20;});
-
-    let temp = new Date(risedt * 1000 + this.timeZone);
-    let riseSecond =
-      temp.getUTCHours() * 3600 + temp.getUTCMinutes() * 60 + temp.getUTCSeconds();
-    let circleGraph = translated.append("g");
-    circleGraph.selectAll("path")
-      .data(dataArr)
-      .enter()
-      .append("path")
-      .attr({
-        fill: (d,i) => {return "rgba("+colors[i]+", 0.0)";}
-      })
-      .transition()
-      .duration(500)
-      .ease("linear")
-      .attr({
-        d: (d,i) => {return arc(d);},
-        transform: (d,i) => {
-          //累計値を計算して，回転角に変換する．
-          let subArr = i;
-          if(i == 0){
-            subArr = [];
-          }else{
-            subArr = dataArr.slice(0, i);
-          }
-          return "translate(25,25),rotate("
-                    + 360 * d3.sum(subArr, actGettingScore) / deySecond +
-                 "),rotate("
-                    + riseScale(riseSecond) +
-                 ")";
-        },
-        stroke: "white",
-        fill: (d,i) => {return "rgba("+colors[i]+", 0.1)";}
-      });
+    this.pieGraphPrint(translated);
   }
 
-  //TODO:今日かそれ以外かで処理を分ける
   /*
    *日付をもらいForecastListを更新し
    *dt:秒単位
@@ -288,6 +129,178 @@ export default class OneDayWeatherForecast{
     console.log(this.forecastList);
   }
 
+  //日付を表示する
+  datePrint(tag){
+    let dateFormat = require('dateformat');
+    let now = tag.append("g");
+    now.selectAll("text")
+      .data([this.forecastList[0]])
+      .enter()
+      .append("text")
+      .attr({
+        x:this.radius + this.margin,
+        y:20
+      })
+      .text((d) => {
+        return dateFormat(new Date(d.dt * 1000 - 1 + this.timeZone)
+               .toUTCString(),"UTC:m/d(ddd)");
+      });
+    return now;
+  }
+  //天気予報のアイコンを表示する
+  forecastIconPrint(tag){
+    let image = tag.append("image");
+    image.attr({
+        x:0,
+        y:0
+      })
+      .transition()
+      .delay((d,i) => {
+        return i * 50;
+      })
+      .duration(500)
+      .ease("linear")
+      .attr({
+        x:(d,i) => {return Math.cos(this.rScale(new Date(d.dt * 1000 + this.timeZone).getUTCHours()) * Math.PI / 180) * this.radius},
+        y:(d,i) => {return Math.sin(this.rScale(new Date(d.dt * 1000 + this.timeZone).getUTCHours()) * Math.PI / 180) * this.radius},
+        href:(d) => {
+          if(typeof d.weather === "undefined"){
+            return "/assets/images/finished-icon.png";
+          }else{
+            let dn = "d";
+            if(!this.isSun(d.dt + this.timeZone / 1000)){
+              dn = "n";
+            }
+            let iconName = d.weather[0].icon
+                          .slice(0,d.weather[0].icon.length - 1)
+                          + dn;
+            return "http://openweathermap.org/img/w/"+iconName+".png";
+          }
+        }
+      });
+    return image;
+  }
+  //天気予報のアイコンを表示する
+  timeTextPrint(tag){
+    let text = tag.append("text");
+    text.attr({
+        x:0,
+        y:0
+      })
+      .transition()
+      .delay((d,i) => {
+        return i * 50;
+      })
+      .duration(500)
+      .ease("linear")
+      .attr({
+        x:(d,i) => {
+          return Math.cos(this.rScale(new Date(d.dt * 1000 + this.timeZone).getUTCHours()) * Math.PI / 180) * (this.radius + 40) + 3;
+        },
+        y:(d,i) => {
+          return Math.sin(this.rScale(new Date(d.dt * 1000 + this.timeZone).getUTCHours()) * Math.PI / 180) * (this.radius + 40) + 30;
+        }
+      })
+      .text((d,i) => {
+        var dateFormat = require('dateformat');
+        return new Date(d.dt * 1000 + this.timeZone).getUTCHours() + ":00";
+      });
+    return text;
+  }
+  //現在の太陽or月を表示する
+  nowIconPrint(tag){
+    let nowIcon = tag.append("image");
+    nowIcon.attr({
+        x:0,
+        y:0
+      })
+      .transition()
+      .duration(500)
+      .ease("linear")
+      .attr({
+        x:() => {
+          return Math.cos(this.rScale(this.date.getUTCHours()) * Math.PI / 180)
+                 * (this.radius - 50);
+        },
+        y:() => {
+          return Math.sin(this.rScale(this.date.getUTCHours()) * Math.PI / 180)
+                 * (this.radius - 50);
+        },
+        href: () => {
+          let tempDate = new Date(this.forecastList[0].dt * 1000 + this.timeZone - 1);
+          if(this.date.getUTCDate() !== tempDate.getUTCDate()){
+            return null;
+          }else if(this.isSun(this.date.getTime() / 1000)){
+            return "http://openweathermap.org/img/w/01d.png";
+          }else{
+            return "http://openweathermap.org/img/w/01n.png";
+          }
+        }
+      });
+    return nowIcon;
+  }
+  //円グラフで日の出、日没を表現する
+  pieGraphPrint(tag){
+    let risedt =this.sunrise;
+    let setdt = this.sunset;
+    let deySecond = 24 * 60 * 60;
+    let dataArr = [
+       {score: setdt - risedt},
+       {score: deySecond - (setdt - risedt)}
+    ];
+    let colors = [
+      "255, 0, 0",
+      "0, 0, 255"
+    ];
+    let riseScale = d3.scale.linear()
+      .domain([0,deySecond])
+      .range([180,540]);
+
+    //合計値を算出する上でのラムダ．
+    let actGettingScore = function(d){return d.score;};
+    //arcオブジェクトは扇形に相当するd操作を生成する
+    let radius = this.radius;
+    let arc = d3.svg.arc()
+      .startAngle(function(d){return 0;})
+      .endAngle(function(d){return Math.PI * 2 * d.score/ deySecond;})
+      .innerRadius(function(d){return 20;})
+      .outerRadius(function(d){return radius + 20;});
+
+    let temp = new Date(risedt * 1000 + this.timeZone);
+    let riseSecond =
+      temp.getUTCHours() * 3600 + temp.getUTCMinutes() * 60 + temp.getUTCSeconds();
+    let circleGraph = tag.append("g");
+    circleGraph.selectAll("path")
+      .data(dataArr)
+      .enter()
+      .append("path")
+      .attr({
+        fill: (d,i) => {return "rgba("+colors[i]+", 0.0)";}
+      })
+      .transition()
+      .duration(500)
+      .ease("linear")
+      .attr({
+        d: (d,i) => {return arc(d);},
+        transform: (d,i) => {
+          //累計値を計算して，回転角に変換する．
+          let subArr = i;
+          if(i == 0){
+            subArr = [];
+          }else{
+            subArr = dataArr.slice(0, i);
+          }
+          return "translate(25,25),rotate("
+                    + 360 * d3.sum(subArr, actGettingScore) / deySecond +
+                 "),rotate("
+                    + riseScale(riseSecond) +
+                 ")";
+        },
+        stroke: "white",
+        fill: (d,i) => {return "rgba("+colors[i]+", 0.1)";}
+      });
+    return circleGraph;
+  }
   /*
    *緯度、経度を入れるとoffsetを返す
    *return offset秒単位
